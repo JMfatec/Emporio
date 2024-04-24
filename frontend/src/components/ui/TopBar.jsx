73c632d3460b8f5e8351b1fb1e68eaf4858c2202
João Marcos <joao.valeriano.silva@gmail.com>
2024-04-24 00:51:10 +0000

    Emporio(23/04)1

--- frontend/src/components/ui/TopBar.jsx
+++ frontend/src/components/ui/TopBar.jsx
@@ -4,12 +4,40 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import logo from '../../assets/montain-logo-600px.jpeg'
 import MainMenu from './MainMenu'
+import myfetch from '../../utils/myfetch'
+import { useLocation, useNavigate } from 'react-router-dom'
+import UserMenu from './UserMenu'
 
 export default function TopBar() {
+
+  const [loggedInUser, setLoggedInUser] = React.useState(null)
+
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  // useEffect() para ser executado quando mudar a rota de front-end
+  React.useEffect(() => {
+    fetchLoggedInUser()
+  }, [location])
+
+  async function fetchLoggedInUser() {
+    try {
+      const user = await myfetch.get('user/loggedin')
+      setLoggedInUser(user)
+    }
+    catch(error) {
+      setLoggedInUser(null)
+      // Se não foi possível obter os dados do usuário autenticado,
+      // redirecionamos para a página de login
+      if(location.pathname !== '/login') navigate('/login')
+    }
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" enableColorOnDark>
+      <AppBar position="static" enableColorOnDark sx={{ justifyContent: 'space-between', flexDirection: 'row' }}>
         <Toolbar variant="dense">
+          
           <MainMenu />
 
           <img
@@ -19,6 +47,9 @@ export default function TopBar() {
           />
           <p style={{ margin: 0, fontSize: '1.5rem' }}>ALPESWERE</p>
         </Toolbar>
+
+        <UserMenu user={loggedInUser} />
+
       </AppBar>
     </Box>
   );
