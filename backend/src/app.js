1d47b4898eb6471beedda91ad57709ce137ecd14
João Marcos <joao.valeriano.silva@gmail.com>
2024-05-23 18:52:34 +0000

    Emporio(23/05)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,23 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-//import dotenv from 'dotenv'
-//import protectRoutes from './lib/protectRotes.js'
-
-import cors from 'cors'
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 import indexRouter from "./rotas/index.js";
 import usersRouter from "./rotas/users.js";
 
 // Importa as variáveis de ambiente do arquivo .env
-//dotenv.config()
+dotenv.config();
+
+//console.log('FRONT_ORIGIN:', process.env.FRONT_ORIGIN);
 
 const app = express();
 
 app.use(cors({
   origin: process.env.FRONT_ORIGIN,
   credentials: true
-}))
+}));
 
 app.use(logger("dev"));
 app.use(json());
@@ -27,22 +27,19 @@ app.use(cookieParser());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-/////////////////////////////////////////////////
-
 // Protege as rotas, exigindo autenticação prévia
+//app.use(protectRoutes);
 
-//app.use(protectRoutes)
+import mesaRouter from './rotas/mesa.js';
+app.use('/mesa', mesaRouter);
 
-import mesaRouter from './rotas/mesa.js'
-app.use('/mesa', mesaRouter)
+import pedidoRouter from './rotas/pedido.js';
+app.use('/pedido', pedidoRouter);
 
-import pedidoRouter from './rotas/pedido.js'
-app.use('/pedido', pedidoRouter)
+import userRouter from './rotas/user.js';
+app.use('/user', userRouter);
 
-import userRouter from './rotas/user.js'
-app.use('/user', userRouter)
-
-import empRouter from './rotas/emp.js'
-app.use('/emp', empRouter)
+import empRouter from './rotas/emp.js';
+app.use('/emp', empRouter);
 
 export default app;
